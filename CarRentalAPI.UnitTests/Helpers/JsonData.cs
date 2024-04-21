db2bedb8b365fccc4cbe3fc32b8ca70bcf686f54
mikolajmusiol <mikolajm575@gmail.com>
2024-04-21 23:33:34 +0200

    Add CheckOverlapTests

---
+++ CarRentalAPI.UnitTests/Helpers/JsonData.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+namespace CarRentalAPI.UnitTests.Helpers
+{
+    public class JsonData : DataAttribute
+    {
+        private readonly string _jsonData;
+
+        public JsonData(string jsonPath)
+        {
+            _jsonData = jsonPath;
+        }
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            if (testMethod == null) throw new ArgumentNullException(nameof(testMethod));
+
+            var currectDir = Directory.GetCurrentDirectory();
+            var jsonFullPath = Path.GetRelativePath(currectDir, _jsonData);
+
+            if (!File.Exists(jsonFullPath))
+            {
+                throw new ArgumentException($"Couldn't find file: {jsonFullPath}");
+            }
+
+            var jsonData = File.ReadAllText(jsonFullPath);
+            var deserialized = JsonConvert.DeserializeObject<IEnumerable<object[]>>(jsonData);
+
+            return deserialized;
+
+        }
+    }
+}
---
+++ CarRentalAPI.UnitTests/UtilitiesTests/CheckOverlapTests.cs
@@ -0,0 +1,31 @@
+﻿using CarRentalAPI.UnitTests.Helpers;
+using CarRentalAPI.Utilities;
+using FluentAssertions;
+using System.Text.Json.Serialization;
+using Xunit;
+
+namespace CarRentalAPI.UnitTests.UtilitiesTests
+{
+    public class CheckOverlapTests
+    {
+        private const string CHECK_OVERLAP_TEST_FOLDER = "TestData/UtilitiesTests/CheckOverlapTests/";
+
+        [Theory]
+        [JsonData(CHECK_OVERLAP_TEST_FOLDER + "ForOverlappingInput.json")]
+        public void OfTwoTimePeriods_ForOverlappingInput_ReturnsTrue(DateTime AStart,
+            DateTime AEnd, DateTime BStart, DateTime BEnd)
+        {
+            var result = CheckOverlap.OfTwoTimePeriods(AStart, AEnd, BStart, BEnd);
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [JsonData(CHECK_OVERLAP_TEST_FOLDER + "ForNotOverlappingInput.json")]
+        public void OfTwoTimePeriods_ForNotOverlappingInput_ReturnsFalse(DateTime AStart,
+            DateTime AEnd, DateTime BStart, DateTime BEnd)
+        {
+            var result = CheckOverlap.OfTwoTimePeriods(AStart, AEnd, BStart, BEnd);
+            result.Should().BeFalse();
+        }
+    }
+}
