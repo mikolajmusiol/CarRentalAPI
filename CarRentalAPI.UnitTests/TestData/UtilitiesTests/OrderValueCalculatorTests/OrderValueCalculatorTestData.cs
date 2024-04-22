b65bba29ae82e7aa805bc7d16a18f0f5001a9d77
mikolajmusiol <mikolajm575@gmail.com>
2024-04-22 22:55:38 +0200

    Add OrderValueCalculatorTests

---
+++ CarRentalAPI.UnitTests/TestData/UtilitiesTests/OrderValueCalculatorTests/OrderValueCalculatorTestData.cs
@@ -0,0 +1,67 @@
+﻿using CarRentalAPI.Entities;
+
+namespace CarRentalAPI.UnitTests.TestData.UtilitiesTests.OrderValueCalculatorTests
+{
+    public class OrderValueCalculatorTestData
+    {
+        public static IEnumerable<object[]> GetPriceForAnHourData()
+        {
+            yield return new object[]
+            {
+                new Order()
+                {
+                    RentalFrom = new DateTime(2000,01,01,12,00,00),
+                    RentalTo = new DateTime(2000,01,01,15,00,00) },
+                new Price()
+                {
+                    PriceForAnHour = 5
+                }
+            };
+        }
+
+        public static IEnumerable<object[]> GetPriceForADayData()
+        {
+            yield return new object[]
+            {
+                new Order()
+                {
+                    RentalFrom = new DateTime(2000,01,01),
+                    RentalTo = new DateTime(2000,01,05) },
+                new Price()
+                {
+                    PriceForADay = 5
+                }
+            };
+        }
+
+        public static IEnumerable<object[]> GetPriceForAWeekData()
+        {
+            yield return new object[]
+            {
+                new Order()
+                {
+                    RentalFrom = new DateTime(2000,01,01),
+                    RentalTo = new DateTime(2000,01,08) },
+                new Price()
+                {
+                    PriceForAWeek = 50
+                }
+            };
+        }
+
+        public static IEnumerable<object[]> GetEmptyPrice()
+        {
+            yield return new object[]
+            {
+                new Order()
+                {
+                    RentalFrom = new DateTime(2000,01,01),
+                    RentalTo = new DateTime(2000,01,08) },
+                new Price()
+                {
+
+                }
+            };
+        }
+    }
+}
---
+++ CarRentalAPI.UnitTests/UtilitiesTests/OrderValueCalculatorTests.cs
@@ -0,0 +1,52 @@
+﻿using CarRentalAPI.Entities;
+using CarRentalAPI.Exceptions;
+using CarRentalAPI.UnitTests.TestData.UtilitiesTests.OrderValueCalculatorTests;
+using CarRentalAPI.Utilities;
+using CarRentalAPI.Utilities.Interfaces;
+using FluentAssertions;
+using Xunit;
+
+namespace CarRentalAPI.UnitTests.UtilitiesTests
+{
+    public class OrderValueCalculatorTests
+    {
+        private readonly IOrderValueCalculator _valueCalculator;
+
+        public OrderValueCalculatorTests()
+        {
+            _valueCalculator = new OrderValueCalculator();
+        }
+
+        [Theory]
+        [MemberData(nameof(OrderValueCalculatorTestData.GetPriceForAnHourData), MemberType = typeof(OrderValueCalculatorTestData))]
+        public void CalculateOrderValue_ForExistingPriceForAnHour_ReturnsOrderValue(Order order, Price price)
+        {
+            var result = _valueCalculator.CalculateOrderValue(order, price);
+            result.Should().Be(15);
+        }
+
+        [Theory]
+        [MemberData(nameof(OrderValueCalculatorTestData.GetPriceForADayData), MemberType = typeof(OrderValueCalculatorTestData))]
+        public void CalculateOrderValue_ForExistingPriceForADay_ReturnsOrderValue(Order order, Price price)
+        {
+            var result = _valueCalculator.CalculateOrderValue(order, price);
+            result.Should().Be(20);
+        }
+
+        [Theory]
+        [MemberData(nameof(OrderValueCalculatorTestData.GetPriceForAWeekData), MemberType = typeof(OrderValueCalculatorTestData))]
+        public void CalculateOrderValue_ForExistingPriceForAWeek_ReturnsOrderValue(Order order, Price price)
+        {
+            var result = _valueCalculator.CalculateOrderValue(order, price);
+            result.Should().Be(50);
+        }
+
+        [Theory]
+        [MemberData(nameof(OrderValueCalculatorTestData.GetEmptyPrice), MemberType = typeof(OrderValueCalculatorTestData))]
+        public void CalculateOrderValue_ForNonExistentPrices_ThrowsException(Order order, Price price)
+        {
+            Action result = () => _valueCalculator.CalculateOrderValue(order, price);
+            result.Should().Throw<NotFoundException>();
+        }
+    }
+}
